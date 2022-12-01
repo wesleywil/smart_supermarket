d88838ca91cb1a728b1072f48c8761409a676d67
wesleywil <wesleywilsonti@gmail.com>
2022-12-01 14:12:31 -0300

    added reactjs and other main dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div classname="bg-red-200">
+      <h1 classname="text-8xl text-white">TEST</h1>
+    </div>
+  );
+}
+
+export default App;
