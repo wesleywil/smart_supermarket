f722452d1bd11b537342d325dfc29f884efedab9
wesleywil <wesleywilsonti@gmail.com>
2022-12-16 16:24:34 -0300

    updated client scan page to fix the qrcode scanner

--- frontend/src/client/components/scan_qr_reader/scan_qr_reader.component.tsx
+++ frontend/src/client/components/scan_qr_reader/scan_qr_reader.component.tsx
@@ -1,61 +1,37 @@
-import QrcodeDecoder from "qrcode-decoder";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
+import { useHtml5QrCodeScanner } from "react-html5-qrcode-reader";
+
+const html5QrCodeScannerFile = "/js/html5-qrcode.min.js";
 
 const ScanQrReader = () => {
-  const qr = new QrcodeDecoder();
-  const video: any = document.querySelector("#video");
-  //   var result: any = document.querySelector("#result");
-  const [result, setResult] = useState("Products information will appear here");
+  const [result, setResult] = useState("");
 
-  const startScan = async () => {
-    alert(qr.getVideoDevices);
-    alert("Scanning...");
-    qr.decodeFromCamera(video).then((res: any) => {
-      console.log(res);
-      setResult(res.data);
-    });
-    // if (!qr.isCanvasSupported()) {
-    //   alert("Your browser doesn't match the required specs.");
-    //   throw new Error("Canvas and getUserMedia are required");
-    // }
-    // const code: any = await qr.decodeFromCamera(video, {
-    //   width: 300,
-    //   height: 300,
-    // });
-    // console.log("code=> ", code);
-
-    // setResult(code.data);
-  };
+  const { Html5QrcodeScanner } = useHtml5QrCodeScanner(html5QrCodeScannerFile);
+  useEffect(() => {
+    if (Html5QrcodeScanner) {
+      let html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader",
+        { fps: 10, qrbox: { width: 250, height: 250 } },
+        /* verbose= */ false
+      );
+      html5QrcodeScanner.render(
+        (data: any) => {
+          setResult(data);
+          console.log("success? ->", data);
+        },
+        (err: any) => console.log("err ->", err)
+      );
+    }
+  }, [Html5QrcodeScanner]);
 
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="mt-2 font-bold text-center w-full overflow-hidden">
-        {result}
-      </h1>
+      <h1 className="w-screen p-2 break-words text-center">{result}</h1>
       <div
-        className="bg-[#1f2630] self-center rounded-xl"
-        style={{ width: 300, height: 300 }}
-      >
-        <video id="video" className="self-center"></video>
-      </div>
-
-      <div className="flex justify-center gap-2 text-[#eeeeeb] ">
-        <button
-          onClick={() => startScan()}
-          className="w-1/3 text-2xl bg-[#8f51b5] hover:bg-[#1f2630] font-bold rounded-xl transition duration-700 ease-in-out"
-        >
-          Start
-        </button>
-        <button
-          className="w-1/3 text-2xl bg-[#8f51b5] hover:bg-[#1f2630] font-bold rounded-xl transition duration-700 ease-in-out"
-          onClick={() => {
-            alert("Stoping CAM");
-            qr.stop();
-          }}
-        >
-          Stop
-        </button>
-      </div>
+        id="reader"
+        className="self-center p-2"
+        style={{ width: "250px", height: "250px" }}
+      ></div>
     </div>
   );
 };
