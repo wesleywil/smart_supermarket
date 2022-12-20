2da7beca7acb5ac6eeb7d292dd005ca8ab20d34a
wesleywil <wesleywilsonti@gmail.com>
2022-12-20 12:54:02 -0300

    added new view to the methods get product by id, update prodcut and delete

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProductViewSet
+from .views import ProductViewSet, ProductDetailView
 
 app_name = 'products_api'
 
 urlpatterns = [
-    path('', ProductViewSet.as_view(), name="product_list")
+    path('', ProductViewSet.as_view(), name="product_list"),
+    path('<int:pk>/', ProductDetailView.as_view(), name="product_detail"),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -18,3 +19,28 @@ class ProductViewSet(APIView):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+class ProductDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
