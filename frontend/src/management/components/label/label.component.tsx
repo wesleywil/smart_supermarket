5f192739ca4a6562b4b03aa0d12e788ebff60068
wesleywil <wesleywilsonti@gmail.com>
2022-12-22 13:19:43 -0300

    updated print_labels area, product tag to be printed can be selected

--- frontend/src/management/components/label/label.component.tsx
+++ frontend/src/management/components/label/label.component.tsx
@@ -1,20 +1,29 @@
-import { FaQrcode } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import type { RootState } from "../../../redux/store";
 
 const Label = () => {
+  const product = useSelector((state: RootState) => state.products.product);
   return (
     <div className="bg-[#eeeeeb] w-96 h-96 p-4 rounded-xl">
       <div className="flex justify-between border-b-2 border-[#8f51b5] pb-2">
         <div className="text-2xl self-center">
-          <h2 className="font-bold">Product Name</h2>
-          <h4 className="text-sm font-bold">Category: category</h4>
+          <h2 className="font-bold">
+            {product ? product.name : "Product Name"}
+          </h2>
+          <h4 className="text-sm font-bold">
+            description: {product ? product.description : ""}
+          </h4>
         </div>
-        <h1 className="self-center text-6xl font-bold">$ 0.00</h1>
+        <h1 className="self-center text-6xl font-bold">
+          {product ? product.price : ""}
+        </h1>
       </div>
       <div className="p-4">
         <img
-          src="https://dummyimage.com/250x250"
+          src={product ? product.qrcode : "https://dummyimage.com/250x250"}
           alt="qrcode"
           className="mx-auto"
+          style={{ width: "250px", height: "250px" }}
         />
       </div>
     </div>
--- frontend/src/management/components/print_labels/print_labels.component.tsx
+++ frontend/src/management/components/print_labels/print_labels.component.tsx
@@ -1,8 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../../../redux/store";
+import { selectProductById } from "../../../redux/management/products/products";
+
 import { FaPlus, FaMinus, FaPrint } from "react-icons/fa";
 
 import Label from "../label/label.component";
+import { useEffect } from "react";
 
 const PrintLabels = () => {
+  const products = useSelector((state: RootState) => state.products.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {}, [products]);
+
+  const selectById = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const id = e.target.value;
+    console.log("VALUE=> ", id);
+    dispatch(selectProductById(+id));
+  };
   return (
     <div className="gap-2 w-full p-4 flex flex-col">
       <h1 className="text-center text-[#eeeeeb] text-6xl underline">
@@ -10,12 +25,15 @@ const PrintLabels = () => {
       </h1>
       <div className="w-fit flex flex-col gap-1 mt-4 border p-4 rounded-xl self-center">
         <h2 className="text-4xl text-[#eeeeeb]">Select Product</h2>
-        <select className="bg-[#8f51b5] text-[#eeeeeb] text-xl font-bold rounded-xl pl-2">
-          <option value="1">Product Name - $0.00</option>
-          <option value="2">Product Name - $0.00</option>
-          <option value="3">Product Name - $0.00</option>
-          <option value="4">Product Name - $0.00</option>
-          <option value="5">Product Name - $0.00</option>
+        <select
+          onChange={selectById}
+          className="bg-[#8f51b5] text-[#eeeeeb] text-xl font-bold rounded-xl pl-2"
+        >
+          {products.map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.name} - ${item.price}
+            </option>
+          ))}
         </select>
       </div>
       <div className="self-center">
