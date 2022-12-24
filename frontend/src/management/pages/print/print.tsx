6588d11c8258972577fbae6b4143801806bc094a
wesleywil <wesleywilsonti@gmail.com>
2022-12-24 12:00:44 -0300

    updating print labels page by adding a print button

--- frontend/src/management/pages/print/print.tsx
+++ frontend/src/management/pages/print/print.tsx
@@ -26,7 +26,18 @@ const Print = () => {
       <h1 className="bg-[#1f2630] text-[#eeeeeb] text-center font-bold text-3xl underline p-2">
         LABELS TO BE PRINTED
       </h1>
-      <div className="flex flex-wrap justify-center gap-1 mt-2">
+      <div className="flex justify-center mt-2">
+        <button
+          onClick={() => {
+            window.print();
+          }}
+          className="bg-[#8f51b5] text-[#eeeeeb] text-xl px-2 py-1 rounded-xl"
+        >
+          Print Labels
+        </button>
+      </div>
+
+      <div id="to_print" className="flex flex-wrap justify-center gap-1 mt-2">
         {rows.map((i) => (
           <Label key={i} />
         ))}
