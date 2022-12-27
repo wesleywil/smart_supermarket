81804e07b2d929f2080c4c1a141c4cb56fd95d66
wesleywil <wesleywilsonti@gmail.com>
2022-12-27 16:29:42 -0300

    added navitation to the navbar

--- frontend/src/components/navbar/navbar.component.tsx
+++ frontend/src/components/navbar/navbar.component.tsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-center gap-2 text-[#eeeeeb] text-2xl">
-      <button className="bg-[#8f51b5] hover:bg-[#1f2630] px-2 rounded-sm  transition duration-700 easy-in">
+      <button
+        onClick={() => navigate("/client/scan")}
+        className="bg-[#8f51b5] hover:bg-[#1f2630] px-2 rounded-sm  transition duration-700 easy-in"
+      >
         Client Area
       </button>
-      <button className="bg-[#8f51b5] hover:bg-[#1f2630] px-2 rounded-sm transition duration-700 easy-in">
+      <button
+        onClick={() => navigate("/admin")}
+        className="bg-[#8f51b5] hover:bg-[#1f2630] px-2 rounded-sm transition duration-700 easy-in"
+      >
         Admin Area
       </button>
     </div>
